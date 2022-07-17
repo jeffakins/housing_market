a46f7278f0c10945bfdd4cf27683f07e5f4599d7
Jeff Akins <jeffakins21@gmail.com>
2022-07-17 00:28:39 -0700

    Added additional charts

--- zillow_charts.py
+++ zillow_charts.py
@@ -22,10 +22,13 @@ import data_transform as dt
 home_prices = pd.read_csv('Metro_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv') # Zillow Home Value Index 
 inventory = pd.read_csv('Metro_invt_fs_uc_sfrcondo_sm_month.csv') # Home Inventory 
 list_sale_price = pd.read_csv('Metro_mlp_uc_sfrcondo_sm_month.csv') # List and Sale Prices
-price_cuts = pd.read_csv('Metro_sales_count_now_uc_sfrcondo_month.csv') # Sales Count and Price Cuts
+price_cuts = pd.read_csv('Metro_perc_listings_price_cut_uc_sfrcondo_sm_week.csv') # Sales Count and Price Cuts
 
 # Data Transforms
 home_pricesT = dt.home_price_transform(home_prices)
+inventoryT = dt.home_price_transform(inventory)
+list_sale_priceT = dt.home_price_transform(list_sale_price)
+price_cutsT = dt.home_price_transform(price_cuts)
 
 # App ----------------------
 app = Dash(__name__, external_stylesheets=[dbc.themes.MORPH])
@@ -39,10 +42,17 @@ app.layout = dbc.Container(
                     placeholder="Select a city",)
         ),
         html.Div(id='city_output_container'),
-        dbc.Row(dcc.Graph(id='home_prices_graph'))
+        dbc.Row(dcc.Graph(id='home_prices_graph')), # Home Value Graph
+        html.H2('Inventory'),
+        dbc.Row(dcc.Graph(id='inventory_graph')), # Inventory Graph
+        html.H2('List Price'),
+        dbc.Row(dcc.Graph(id='list_price_graph')), # List Price Graph
+        html.H2('Price Cut'),
+        dbc.Row(dcc.Graph(id='price_cut_graph')), # Price Cut Graph
     ],
     fluid=True,
 )
+# Callback for Home Value Estimate
 @app.callback(
     Output(component_id='home_prices_graph', component_property='figure'), # Output graph
     Input(component_id='city_selection', component_property='value') # Input dropdown city selections
@@ -56,6 +66,47 @@ def update_graph(cities):
                    height=600,)
     return fig
 
+# Callback for Inventory Levels
+@app.callback(
+    Output(component_id='inventory_graph', component_property='figure'), # Output graph
+    Input(component_id='city_selection', component_property='value') # Input dropdown city selections
+)
+def update_graph(cities):
+    home_selection = inventoryT[cities] # New dataframe with only columns from selection
+    fig = px.line(home_selection, x=home_selection.index, y=home_selection.columns) # Graph with new dataframe
+    fig.update_layout(title='Home Inventory Levels',
+                   xaxis_title='Year',
+                   yaxis_title='Number of Homes for Sale',
+                   height=600,)
+    return fig
+
+# Callback for List Price
+@app.callback(
+    Output(component_id='list_price_graph', component_property='figure'), # Output graph
+    Input(component_id='city_selection', component_property='value') # Input dropdown city selections
+)
+def update_graph(cities):
+    home_selection = list_sale_priceT[cities] # New dataframe with only columns from selection
+    fig = px.line(home_selection, x=home_selection.index, y=home_selection.columns) # Graph with new dataframe
+    fig.update_layout(title='Median List Price',
+                   xaxis_title='Year',
+                   yaxis_title='Price',
+                   height=600,)
+    return fig
+
+# Callback for Price Cuts
+@app.callback(
+    Output(component_id='price_cut_graph', component_property='figure'), # Output graph
+    Input(component_id='city_selection', component_property='value') # Input dropdown city selections
+)
+def update_graph(cities):
+    home_selection = price_cutsT[cities] # New dataframe with only columns from selection
+    fig = px.line(home_selection, x=home_selection.index, y=home_selection.columns) # Graph with new dataframe
+    fig.update_layout(title='Mean Price Cut of Homes per Region',
+                   xaxis_title='Year',
+                   yaxis_title='Price Cut Amount',
+                   height=600,)
+    return fig    
 
 if __name__ == '__main__':
     app.run_server(debug=True)
