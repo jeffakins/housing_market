0afafbff244cdadd404872741473e084af45392b
Jeff Akins <jeffakins21@gmail.com>
2022-08-06 22:08:18 -0700

    Modified Charts

--- zillow_charts.py
+++ zillow_charts.py
@@ -21,17 +21,19 @@ import data_transform as dt
 # Data Imports
 home_prices = pd.read_csv('Metro_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv') # Zillow Home Value Index 
 inventory = pd.read_csv('Metro_invt_fs_uc_sfrcondo_week.csv') # Home Inventory 
-list_sale_price = pd.read_csv('Metro_mlp_uc_sfrcondo_sm_month.csv') # List and Sale Prices
+list_sale_price = pd.read_csv('Metro_median_sale_price_uc_sfrcondo_week.csv') # List and Sale Prices
+list_to_sale = pd.read_csv('Metro_mean_sale_to_list_uc_sfrcondo_week.csv') # Mean list to dales ratio
 price_cuts = pd.read_csv('Metro_perc_listings_price_cut_uc_sfrcondo_week.csv') # Price Cuts
-days_to_close = pd.read_csv('Metro_pct_sold_below_list_uc_sfrcondo_week.csv') # Mean days to Close
+percent_below_list = pd.read_csv('Metro_pct_sold_below_list_uc_sfrcondo_week.csv') # Percent of Home Sold below List Price
 rent = pd.read_csv('Metro_ZORI_AllHomesPlusMultifamily_Smoothed.csv') # Rental Prices
 
 # Data Transforms
 home_pricesT = dt.home_price_transform(home_prices)
 inventoryT = dt.home_price_transform(inventory)
 list_sale_priceT = dt.home_price_transform(list_sale_price)
+list_to_saleT = dt.home_price_transform(list_to_sale)
 price_cutsT = dt.home_price_transform(price_cuts)
-days_to_closeT = dt.home_price_transform(days_to_close)
+percent_below_listT = dt.home_price_transform(percent_below_list)
 rentT = dt.home_rent_transform(rent)
 
 # App ----------------------
@@ -49,12 +51,14 @@ app.layout = dbc.Container(
         dbc.Row(dcc.Graph(id='home_prices_graph')), # Home Value Graph
         html.H2('Inventory'),
         dbc.Row(dcc.Graph(id='inventory_graph')), # Inventory Graph
-        html.H2('List Price'),
+        html.H2('Sale Price'),
         dbc.Row(dcc.Graph(id='list_price_graph')), # List Price Graph
+        html.H2('Sale to List Ratio'),
+        dbc.Row(dcc.Graph(id='list_to_sale_graph')), # List to Sale Graph
         html.H2('Price Cut'),
         dbc.Row(dcc.Graph(id='price_cut_graph')), # Price Cut Graph
-        html.H2('Days to Close'),
-        dbc.Row(dcc.Graph(id='days_to_close_graph')), # Days to close Graph
+        html.H2('Percent of Homes that Sold Below List Price'),
+        dbc.Row(dcc.Graph(id='percent_below_list_graph')), # Percent below list
         html.H2('Rent'),
         dbc.Row(dcc.Graph(id='rent_graph')), # Rental Rates Graph
     ],
@@ -88,7 +92,7 @@ def update_graph(cities):
                    height=600,)
     return fig
 
-# Callback for List Price
+# Callback for Sale Price
 @app.callback(
     Output(component_id='list_price_graph', component_property='figure'), # Output graph
     Input(component_id='city_selection', component_property='value') # Input dropdown city selections
@@ -96,9 +100,23 @@ def update_graph(cities):
 def update_graph(cities):
     home_selection = list_sale_priceT[cities] # New dataframe with only columns from selection
     fig = px.line(home_selection, x=home_selection.index, y=home_selection.columns) # Graph with new dataframe
-    fig.update_layout(title='Median List Price',
+    fig.update_layout(title='Median Sale Price (All homes, weekly)',
                    xaxis_title='Date',
                    yaxis_title='Price ($)',
+                   height=600,)
+    return fig
+
+# Callback for List to Sale
+@app.callback(
+    Output(component_id='list_to_sale_graph', component_property='figure'), # Output graph
+    Input(component_id='city_selection', component_property='value') # Input dropdown city selections
+)
+def update_graph(cities):
+    home_selection = list_to_saleT[cities] # New dataframe with only columns from selection
+    fig = px.line(home_selection, x=home_selection.index, y=home_selection.columns) # Graph with new dataframe
+    fig.update_layout(title='Mean Sale to List Ratio (All homes, weekly)',
+                   xaxis_title='Date',
+                   yaxis_title='Sale to List',
                    height=600,)
     return fig
 
@@ -110,23 +128,23 @@ def update_graph(cities):
 def update_graph(cities):
     home_selection = price_cutsT[cities] # New dataframe with only columns from selection
     fig = px.line(home_selection, x=home_selection.index, y=home_selection.columns) # Graph with new dataframe
-    fig.update_layout(title='Share of Listings with a Price Cut (all home types)',
+    fig.update_layout(title='Share of Listings with a Price Cut (All home types)',
                    xaxis_title='Date',
                    yaxis_title='Percent of Homes with a Price Cut (%)',
                    height=600,)
     return fig  
 
-# Callback for Median Days to Close
+# Percent of Homes Sold Below the List Price
 @app.callback(
-    Output(component_id='days_to_close_graph', component_property='figure'), # Output graph
+    Output(component_id='percent_below_list_graph', component_property='figure'), # Output graph
     Input(component_id='city_selection', component_property='value') # Input dropdown city selections
 )
 def update_graph(cities):
-    home_selection = days_to_closeT[cities] # New dataframe with only columns from selection
+    home_selection = percent_below_listT[cities] # New dataframe with only columns from selection
     fig = px.line(home_selection, x=home_selection.index, y=home_selection.columns) # Graph with new dataframe
-    fig.update_layout(title='Median Days to Close',
+    fig.update_layout(title='Percent of Homes Sold Below the List Price',
                    xaxis_title='Date',
-                   yaxis_title='Days',
+                   yaxis_title='Percent of Homes Sold below List',
                    height=600,)
     return fig   
 
@@ -138,7 +156,7 @@ def update_graph(cities):
 def update_graph(cities):
     home_selection = rentT[cities] # New dataframe with only columns from selection
     fig = px.line(home_selection, x=home_selection.index, y=home_selection.columns) # Graph with new dataframe
-    fig.update_layout(title='Rental Rate per Region',
+    fig.update_layout(title='Monthly Rental Rate per Region',
                    xaxis_title='Date',
                    yaxis_title='Rental Rate ($)',
                    height=600,)
